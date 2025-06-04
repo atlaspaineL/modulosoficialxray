612c2a7522beb08026ede3d8748ba1e16bd23a49
ATLASPAINEL <51466906+atlaspaineL@users.noreply.github.com>
2025-06-04 14:12:40 -0300

    Update modulosinstall.sh

--- modulosinstall.sh
+++ modulosinstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 rm -f atlasdata.sh atlascreate.sh atlasteste.sh atlasremove.sh delete.py sincronizar.py add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
-wget -O atlascreate.sh "https://almirantessh.top/modulos/atlascreate.sh"
-wget -O add.sh "https://almirantessh.top/modulos/add.sh"
-wget -O remsinc.sh "https://almirantessh.top/modulos/remsinc.sh"
-wget -O addsinc.sh "https://almirantessh.top/modulos/addsinc.sh"
-wget -O rem.sh "https://almirantessh.top/modulos/rem.sh"
-wget -O atlasteste.sh "https://almirantessh.top/modulos/atlasteste.sh"
-wget -O addteste.sh "https://almirantessh.top/modulos/addteste.sh"
-wget -O atlasremove.sh "https://almirantessh.top/modulos/atlasremove.sh"
-wget -O delete.py "https://almirantessh.top/modulos/delete.py"
-wget -O atlasdata.sh "https://almirantessh.top/modulos/atlasdata.sh"
-wget -O sincronizar.py "https://almirantessh.top/modulos/sincronizar.py"
+wget -O atlascreate.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/atlascreate.sh"
+wget -O add.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/add.sh"
+wget -O remsinc.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/remsinc.sh"
+wget -O addsinc.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/addsinc.sh"
+wget -O rem.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/rem.sh"
+wget -O atlasteste.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/atlasteste.sh"
+wget -O addteste.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/addteste.sh"
+wget -O atlasremove.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/atlasremove.sh"
+wget -O delete.py "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/delete.py"
+wget -O atlasdata.sh "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/atlasdata.sh"
+wget -O sincronizar.py "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/sincronizar.py"
 chmod 777 atlascreate.sh add.sh remsinc.sh addsinc.sh rem.sh atlasteste.sh addteste.sh atlasremove.sh delete.py atlasdata.sh sincronizar.py
 sudo apt install dos2unix
 dos2unix rem.sh
-wget "https://almirantessh.top/modulos/verificador.py" -O verificador.py 
+wget "https://raw.githubusercontent.com/atlaspaineL/modulosoficialxray/main/verificador.py" -O verificador.py 
 sudo python3 verificador.py
